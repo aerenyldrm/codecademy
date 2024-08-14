5cd4b3bae09eb62b54b7c889acbbaaf8b74b7cab
aerenyldrm <aerenyldrm@outlook.com>
2024-08-14 08:54:14 +0300

    update

--- slice_of_len.py
+++ slice_of_len.py
@@ -1,4 +1,3 @@
-# Your code below:
 toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchoives", "mushrooms"]
 prices = [2, 6, 1, 3, 2, 7, 2]
 num_two_dollar_slices = prices.count(2)
